371d463d9c2616d69a125ef82f899931ddfb58e6
Sebastian Alfieri <sebasalfieri@gmail.com>
2025-05-09 20:16:09 -0300

    contact

--- components/SectionContact/index.tsx
+++ components/SectionContact/index.tsx
@@ -1,9 +1,15 @@
+"use client";
 import React from "react";
 import { motion } from "framer-motion";
 import s from "./SectionContact.module.css";
 import { LineNumbers } from "@/components";
+import { ReactSVG } from "react-svg";
 
 const SectionContact = () => {
+  const handleLink = (link: string) => {
+    window.open(link, "_blank");
+  };
+
   return (
     <motion.section
       className={s.container}
@@ -13,6 +19,53 @@ const SectionContact = () => {
     >
       <LineNumbers>
         <h2>Contact me</h2>
+        <div className={s.container__buttons}>
+          <button
+            className={s.container__buttons__wsp}
+            onClick={() => handleLink("https://wa.me/5491122251843")}
+          >
+            <ReactSVG
+              src="/icons/whatsapp.svg"
+              wrapper="span"
+              className={s.container__buttons__wsp__icon}
+            />
+          </button>
+          <button
+            className={s.container__buttons__email}
+            onClick={() => handleLink("mailto:sebasalfieri@gmail.com")}
+          >
+            {" "}
+            <ReactSVG
+              src="/icons/email.svg"
+              wrapper="span"
+              className={s.container__buttons__email__icon}
+            />
+          </button>
+          <button
+            className={s.container__buttons__github}
+            onClick={() => handleLink("https://github.com/SebasAlfieri")}
+          >
+            {" "}
+            <ReactSVG
+              src="/icons/github.svg"
+              wrapper="span"
+              className={s.container__buttons__github__icon}
+            />
+          </button>
+          <button
+            className={s.container__buttons__linkedin}
+            onClick={() =>
+              handleLink("https://www.linkedin.com/in/sebastianalfieri/")
+            }
+          >
+            {" "}
+            <ReactSVG
+              src="/icons/linkedin.svg"
+              wrapper="span"
+              className={s.container__buttons__linkedin__icon}
+            />
+          </button>
+        </div>
       </LineNumbers>
     </motion.section>
   );
